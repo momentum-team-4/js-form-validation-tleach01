cafdb8193c36ffe60200134b770aaec9d34b3cc0
tleach01 <taylorleach0@gmail.com>
2020-08-23 19:34:46 -0400

    back to five

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ let submit = document.querySelector('#submit-button');
 let totalCost = document.querySelector('#total')
 
 
+
 submit.addEventListener('click', function(){
-event.preventDefault();
 let numOfDays = document.querySelector('#days').value
 let cost = (numOfDays * 5)
-
 totalCost.innerHTML = 'Total cost is: ' + '$ ' + cost;
 }
 )
 
+
+//
 
 
