471ffca8d98f5ac09db7b6fad1a7898320a61c2b
tleach01 <taylorleach0@gmail.com>
2020-08-24 08:53:44 -0400

    still number 5 issues

--- script.js
+++ script.js
@@ -15,3 +15,25 @@ totalCost.innerHTML = 'Total cost is: ' + '$ ' + cost;
 //
 
 
+function validateCardNumber(number) {
+    var regex = new RegExp("^[0-9]{16}$");
+    if (!regex.test(number))
+        return false;
+
+    return luhnCheck(number);
+}
+
+function luhnCheck(val) {
+    var sum = 0;
+    for (var i = 0; i < val.length; i++) {
+        var intVal = parseInt(val.substr(i, 1));
+        if (i % 2 == 0) {
+            intVal *= 2;
+            if (intVal > 9) {
+                intVal = 1 + (intVal % 10);
+            }
+        }
+        sum += intVal;
+    }
+    return (sum % 10) == 0;
+}
