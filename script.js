4cf18181df2eed42c22b46b84ba4741d2f98360a
tleach01 <taylorleach0@gmail.com>
2020-08-24 22:36:04 -0400

    working on step seven

--- script.js
+++ script.js
@@ -1,39 +1,65 @@
+const submit = document.querySelector('#submit-button')
+const totalCost = document.querySelector('#total')
 
-let submit = document.querySelector('#submit-button');
-let totalCost = document.querySelector('#total')
-
-
-
-submit.addEventListener('click', function(){
-let numOfDays = document.querySelector('#days').value
-let cost = (numOfDays * 5)
-totalCost.innerHTML = 'Total cost is: ' + '$ ' + cost;
+submit.addEventListener('click', function (event) {
+  event.preventDefault()
+  const cost = price()
+  totalCost.innerHTML = 'Total cost is: $ ' + cost
 }
 )
+// new prices //
 
+const today = new Date()
 
-//
-
-
-function validateCardNumber(number) {
-    var regex = new RegExp("^[0-9]{16}$");
-    if (!regex.test(number))
-        return false;
-
-    return luhnCheck(number);
-}
-
-function luhnCheck(val) {
-    var sum = 0;
-    for (var i = 0; i < val.length; i++) {
-        var intVal = parseInt(val.substr(i, 1));
-        if (i % 2 == 0) {
-            intVal *= 2;
-            if (intVal > 9) {
-                intVal = 1 + (intVal % 10);
-            }
-        }
-        sum += intVal;
+function price() {
+  const dayHolder = document.querySelector('#start-date').value
+  starting = new Date(dayHolder + 'T00:00')
+  const weekDay = starting.getDay()
+  const numOfDays = document.querySelector('#days').value
+  let priceAdjust = 0
+  for (i = 0; i < numOfDays; i++) {
+    const check = weekDay + i + 1
+    if ((check % 7) > 1) {
+      priceAdjust += 5
+    } else {
+      priceAdjust += 7
     }
-    return (sum % 10) == 0;
+  }
+  return priceAdjust
 }
+
+// validate cc //
+
+function validateCardNumber (number) {
+  var regex = new RegExp('^[0-9]{16}$')
+  if (!regex.test(number))
+    return false
+
+  return luhnCheck(number)
+}
+
+function luhnCheck (val) {
+  var sum = 0
+  for (var i = 0; i < val.length; i++) {
+    var intVal = parseInt(val.substr(i, 1))
+    if (i % 2 === 0) {
+      intVal *= 2
+      if (intVal > 9) {
+        intVal = 1 + (intVal % 10)
+      }
+    }
+    sum += intVal
+  }
+  return (sum % 10) === 0
+}
+
+const creditCardNum = document.querySelector('#credit-card')
+creditCardNum.addEventListener('focusout', function () {
+  if (validateCardNumber(document.querySelector('#credit-card').value)) {
+    creditCardNum.setCustomValidity('')
+  } else {
+    creditCardNum.setCustomValidity('Please enter a valid credit card number!')
+  }
+})
+
+//  //
